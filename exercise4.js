ef83fe89ce4e8a8d92454d2f05aa63c55cc742c1
Jamie Long <jtlong3rd@gmail.com>
2016-04-30 19:46:12 -0400

    Refactor Exercise 4 (MY FIRST ASYNC I/O!)

--- exercise4.js
+++ exercise4.js
@@ -10,10 +10,10 @@
 var fs = require('fs');
 var filePath = process.argv[2];
 
-fs.readFile(filePath, 
-            function(err, data) {
-                if (err) throw err;
-                
-                console.log(data.toString().split('\n').length - 1);
-            }
-            );
+fs.readFile(filePath, function(err, data) {
+  if (err) {
+    throw err;
+  }
+  
+  console.log(data.toString().split('\n').length - 1);
+});
