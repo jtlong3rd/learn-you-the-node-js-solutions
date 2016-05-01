181f2355390655b51de8362ca2a9c8601c9431fc
Jamie Long <jtlong3rd@gmail.com>
2016-04-30 20:05:57 -0400

    Refactor Exercise 8 (HTTP COLLECT)

--- exercise8.js
+++ exercise8.js
@@ -12,12 +12,14 @@
 var http = require('http');
 var url = process.argv[2];
 
-http.get(url, 
-         function (response) {
-             var output = '';
-             
-             response.setEncoding('utf8');
-             response.on('data', function(data) { output += data; });
-             response.on('end', function(data) { console.log(output.length + '\n' + output); });
-         }
-        );
+http.get(url, function (response) {
+  var output = '';
+  response.setEncoding('utf8');
+  response.on('data', function(data) { 
+    output += data; 
+  });
+  response.on('error', console.error);
+  response.on('end', function(data) { 
+    console.log(output.length + '\n' + output); 
+  });
+});
