3ca8ff51ed7cbfb5e3623b14a287bbe170561d6a
jtlong3rd <jtlong3rd@gmail.com>
2016-11-12 17:12:25 -0500

    Refactor Exercise 3 to use object destructuring

--- exercise3.js
+++ exercise3.js
@@ -1,18 +1,14 @@
 // MY FIRST I/O!
 //
-// PROMPT: Write a program that uses a single synchronous filesystem operation to  
-// read a file and print the number of newlines (\n) it contains to the  
-// console (stdout), similar to running cat file | wc -l.  
-   
-// The full path to the file to read will be provided as the first  
-// command-line argument (i.e., process.argv[2]). You do not need to make  
-// your own test file. 
+// PROMPT: Write a program that uses a single synchronous filesystem operation to
+// read a file and print the number of newlines (\n) it contains to the
+// console (stdout), similar to running cat file | wc -l.
 
-var fs = require('fs');
-var filePath = process.argv[2];
+// The full path to the file to read will be provided as the first
+// command-line argument (i.e., process.argv[2]). You do not need to make
+// your own test file.
 
-var inputBuf = fs.readFileSync(filePath);
-var inputStr = inputBuf.toString();
-var inputLinesArr = inputStr.split('\n');
+const { readFileSync } = require('fs');
+const filePath = process.argv[2];
 
-console.log(inputLinesArr.length - 1);
+console.log(readFileSync(filePath).toString().split('\n').length - 1);
