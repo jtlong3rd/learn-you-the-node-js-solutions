a1306aa1822d8c0a9b0a182a8b9e3a1fe9c40a33
jtlong3rd <jtlong3rd@gmail.com>
2016-11-12 17:04:35 -0500

    Add newline to Exercise 1 solution

--- exercise1.js
+++ exercise1.js
@@ -1,6 +1,6 @@
 // HELLO WORLD
 //
-// PROMPT: Write a program that prints the text "HELLO WORLD" to the console  
+// PROMPT: Write a program that prints the text "HELLO WORLD" to the console
 // (stdout).
 
 console.log('HELLO WORLD');
