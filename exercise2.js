69a61b5e2da5182bbd767cf750fa5f324ee0ac7e
jtlong3rd <jtlong3rd@gmail.com>
2016-11-12 17:08:57 -0500

    Refactor Exercise 2 using arrow functions

--- exercise2.js
+++ exercise2.js
@@ -1,8 +1,6 @@
 // BABY STEPS
 //
-// PROMPT: Write a program that accepts one or more numbers as command-line arguments  
-// and prints the sum of those numbers to the console (stdout). 
+// PROMPT: Write a program that accepts one or more numbers as command-line arguments
+// and prints the sum of those numbers to the console (stdout).
 
-console.log(process.argv.slice(2).reduce(function(sum, nextItem) {
-  return Number(sum) + Number(nextItem);
-}));
+console.log(process.argv.slice(2).reduce((sum, item) => sum + Number(item), 0));
