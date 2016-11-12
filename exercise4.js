a1a81ebd81f60c8a34e8f87fcaea46443973241c
jtlong3rd <jtlong3rd@gmail.com>
2016-11-12 17:21:14 -0500

    Refactor Exercise 4 to use ES6

--- exercise4.js
+++ exercise4.js
@@ -1,19 +1,19 @@
 // MY FIRST ASYNC I/O!
 //
-// PROMPT: Write a program that uses a single asynchronous filesystem operation to  
-// read a file and print the number of newlines it contains to the console  
-// (stdout), similar to running cat file | wc -l.  
-   
-// The full path to the file to read will be provided as the first  
-// command-line argument.  
+// PROMPT: Write a program that uses a single asynchronous filesystem operation to
+// read a file and print the number of newlines it contains to the console
+// (stdout), similar to running cat file | wc -l.
 
-var fs = require('fs');
-var filePath = process.argv[2];
+// The full path to the file to read will be provided as the first
+// command-line argument.
 
-fs.readFile(filePath, function(err, data) {
+const { readFile } = require('fs');
+const filePath = process.argv[2];
+
+readFile(filePath, (err, data) => {
   if (err) {
     throw err;
   }
-  
+
   console.log(data.toString().split('\n').length - 1);
 });
