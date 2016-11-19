34e3d104c854a21f01ff1bc0e4de52847843e972
jtlong3rd <jtlong3rd@gmail.com>
2016-11-19 17:42:04 -0500

    Refactor Exercise 8 using arrow functions and ES6 lets/consts

--- exercise8.js
+++ exercise8.js
@@ -1,25 +1,28 @@
 // HTTP COLLECT
-//   
-// PROMPT: Write a program that performs an HTTP GET request to a URL provided to you  
-// as the first command-line argument. Collect all data from the server (not  
-// just the first "data" event) and then write two lines to the console  
-// (stdout).  
-//   
-// The first line you write should just be an integer representing the number  
-// of characters received from the server. The second line should contain the  
-// complete String of characters sent by the server.  
+//
+// PROMPT: Write a program that performs an HTTP GET request to a URL provided to you
+// as the first command-line argument. Collect all data from the server (not
+// just the first "data" event) and then write two lines to the console
+// (stdout).
+//
+// The first line you write should just be an integer representing the number
+// of characters received from the server. The second line should contain the
+// complete String of characters sent by the server.
 
-var http = require('http');
-var url = process.argv[2];
+const { get } = require('http');
+const url = process.argv[2];
 
-http.get(url, function (response) {
-  var output = '';
+get(url, (response) => {
+  let output = '';
   response.setEncoding('utf8');
-  response.on('data', function(data) { 
-    output += data; 
+
+  response.on('data', function(data) {
+    output += data;
   });
+
   response.on('error', console.error);
-  response.on('end', function(data) { 
-    console.log(output.length + '\n' + output); 
+
+  response.on('end', function(data) {
+    console.log(output.length + '\n' + output);
   });
 });
