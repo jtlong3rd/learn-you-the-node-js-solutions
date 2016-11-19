04fcb1988755e77978b9b2a033f88b8d30a13ae7
jtlong3rd <jtlong3rd@gmail.com>
2016-11-19 17:20:02 -0500

    Refactor Exercise 7 using object destructuring

--- exercise7.js
+++ exercise7.js
@@ -1,13 +1,13 @@
 // HTTP CLIENT
-//   
-// PROMPT: Write a program that performs an HTTP GET request to a URL provided to you  
-// as the first command-line argument. Write the String contents of each  
-// "data" event from the response to a new line on the console (stdout). 
+//
+// PROMPT: Write a program that performs an HTTP GET request to a URL provided to you
+// as the first command-line argument. Write the String contents of each
+// "data" event from the response to a new line on the console (stdout).
 
-var http = require('http');
+var { get } = require('http');
 var url = process.argv[2];
 
-http.get(url, function (response) {
+get(url, (response) => {
   response.setEncoding('utf8');
   response.on('data', console.log);
   response.on('error', console.error);
