39bf58e10786229574264ec8b734e4afd4a52822
jtlong3rd <jtlong3rd@gmail.com>
2016-11-19 20:43:13 -0500

    Refactor Exercise 10 using ES6 consts and arrow functions

--- exercise10.js
+++ exercise10.js
@@ -1,27 +1,25 @@
 // TIME SERVER
 //
-// PROMPT: Write a TCP time server!  
+// PROMPT: Write a TCP time server!
 //
-// Your server should listen to TCP connections on the port provided by the  
-// first argument to your program. For each connection you must write the  
-// current date & 24 hour time in the format:  
-//  
-//    "YYYY-MM-DD hh:mm"  
-//   
-// followed by a newline character. Month, day, hour and minute must be  
-// zero-filled to 2 integers. For example:  
-//  
-//    "2013-07-06 17:42" 
+// Your server should listen to TCP connections on the port provided by the
+// first argument to your program. For each connection you must write the
+// current date & 24 hour time in the format:
+//
+//    "YYYY-MM-DD hh:mm"
+//
+// followed by a newline character. Month, day, hour and minute must be
+// zero-filled to 2 integers. For example:
+//
+//    "2013-07-06 17:42"
 
-var net = require('net');
-var port = process.argv[2];
+const { createServer } = require('net');
+const port = process.argv[2];
 
-var zeroPad = function(param) {
-  return param < 10 ? '0' + param : param;
-}
+const zeroPad = param => param < 10 ? '0' + param : param;
 
-var server = net.createServer(function(socket){
-  var currTime = new Date();
+const server = createServer(socket => {
+  const currTime = new Date();
 
   socket.write(currTime.getFullYear() + '-' +
     zeroPad(currTime.getMonth() + 1) + '-' +
