fbef0510dd1be1cbadbe104ee0ddb8d1e57d5800
jtlong3rd <jtlong3rd@gmail.com>
2016-11-20 19:20:29 -0500

    Refactor Exercise 11 using ES6 arrow functions

--- exercise11.js
+++ exercise11.js
@@ -1,22 +1,22 @@
-// HTTP FILE SERVER 
-//   
-// PROMPT: Write an HTTP server that serves the same text file for each request it  
-// receives.  
-//   
-// Your server should listen on the port provided by the first argument to  
-// your program.  
-//   
-// You will be provided with the location of the file to serve as the second  
-// command-line argument. You must use the fs.createReadStream() method to  
-// stream the file contents to the response.  
+// HTTP FILE SERVER
+//
+// PROMPT: Write an HTTP server that serves the same text file for each request it
+// receives.
+//
+// Your server should listen on the port provided by the first argument to
+// your program.
+//
+// You will be provided with the location of the file to serve as the second
+// command-line argument. You must use the fs.createReadStream() method to
+// stream the file contents to the response.
 
-var fs = require('fs');
-var http = require('http');
-var port = process.argv[2];
-var filePath = process.argv[3];
+const { createReadStream } = require('fs');
+const { createServer }= require('http');
+const port = process.argv[2];
+const filePath = process.argv[3];
 
-var server = http.createServer(function(request, response) {
-  var fileStream = fs.createReadStream(filePath);
+const server = createServer((request, response) => {
+  const fileStream = createReadStream(filePath);
 
   response.writeHead(200, {contentType: 'text/plain'});
   fileStream.pipe(response);
