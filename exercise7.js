f80f415d639dbffac72192194ac7d31fb457bd44
jtlong3rd <jtlong3rd@gmail.com>
2016-11-20 19:35:07 -0500

    Remove optional parentheses from single-parameter arrow function

--- exercise7.js
+++ exercise7.js
@@ -7,7 +7,7 @@
 const { get } = require('http');
 const url = process.argv[2];
 
-get(url, (response) => {
+get(url, response => {
   response.setEncoding('utf8');
   response.on('data', console.log);
   response.on('error', console.error);
